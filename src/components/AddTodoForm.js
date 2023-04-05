fe3c86d61c7cf89065ba00ec5ac4f61318eca40d
Count-MonteCristo <luis0425navarro@gmail.com>
2023-04-04 19:53:50 -0500

    Performance, TypeScript, Testing, Project Structure

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import InputWithLabel from "./InputWithLabel";
 import style from "./AddTodoForm.module.css";
+import PropTypes from "prop-types";
 
 function AddTodoForm({ onAddTodo }) {
   const [todoTitle, setTodoTitle] = useState([]);
@@ -32,5 +33,9 @@ function AddTodoForm({ onAddTodo }) {
     </form>
   );
 }
+
+AddTodoForm.propTypes = {
+  onAddTodo: PropTypes.func,
+};
 
 export default AddTodoForm;
